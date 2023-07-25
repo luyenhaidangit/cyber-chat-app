d82a786bbd3c6e6d118f2277fb2f882c34ccebb4
luyenhaidangit <luyenhaidangit@gmail.com>
2023-07-25 09:48:25 +0700

    Fix bug continue login user not verify email

--- app/Http/Requests/Guest/LoginRequest.php
+++ app/Http/Requests/Guest/LoginRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Guest;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\User;
 
 class LoginRequest extends FormRequest
 {
@@ -14,7 +16,7 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|email|exists:users,email,email_verified_at,NULL',
+            'email' => 'required|email|exists:users,email',
             'password' => 'required|min:6',
         ];
     }
@@ -24,9 +26,19 @@ class LoginRequest extends FormRequest
         return [
             'email.required' => 'Vui lòng nhập địa chỉ email.',
             'email.email' => 'Vui lòng nhập đúng định dạng email.',
-            'email.exists' => 'Email không tồn tại trong hệ thống hoặc chưa được xác nhận.',
+            'email.exists' => 'Email không tồn tại trong hệ thống.',
             'password.required' => 'Vui lòng nhập mật khẩu mới.',
             'password.min' => 'Mật khẩu mới phải có ít nhất 6 ký tự.',
         ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $user = User::where('email', $this->email)->first();
+            if ($user && is_null($user->email_verified_at)) {
+                $validator->errors()->add('email', 'Email chưa được xác thực.');
+            }
+        });
     }
 }
